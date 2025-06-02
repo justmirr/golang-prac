e8e28fc0402068b4b402e4e53ca31ac45a87a14b
Mir <moazinmir@gmail.com>
2025-06-02 11:39:16 +0530

    Complete Slices

--- 13-slices/main.go
+++ 13-slices/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"slices"
+)
 
 func main() {
 	var one []int
@@ -8,12 +11,19 @@ func main() {
 	var two = make([]int, 2)
 	fmt.Println(two, len(two))
 
-	two[0] = 50
-	two[1] = 100
+	two[0] = 1
+	two[1] = 2
 	two = append(two, 150)
 
 	fmt.Println(two, len(two))
 	var three = make([]int, len(two))
 	copy(three, two)
 	fmt.Println(three, len(three))
+
+	var four = []int{1, 2}
+	fmt.Println(four[:])
+	fmt.Println(slices.Equal(two, three))
+
+	var five = [][]int{{1, 2, 3}, {1, 2, 3}}
+	fmt.Println(five)
 }
