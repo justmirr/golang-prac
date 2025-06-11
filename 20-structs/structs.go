2ede7b1ab0c331d16ab036f1cdd6fb930d875943
Mir <moazinmir@gmail.com>
2025-06-11 11:08:53 +0530

    Constructors using Struct

--- 20-structs/structs.go
+++ 20-structs/structs.go
@@ -8,19 +8,41 @@ type student struct {
 	marks float64
 }
 
-func main() {
-	one := student{
-		id:    111,
-		name:  "Mir",
-		marks: 214.22,
-	}
-	fmt.Println(one)
+func (s *student) changeMarks(marks float64) {
+	s.marks = marks
+}
 
-	two := student{
-		id:    7,
-		name:  "Bond",
-		marks: 239.4,
+func newStudent(id int, name string, marks float64) *student {
+	one := student{
+		id:    id,
+		name:  name,
+		marks: marks,
 	}
+	return &one
+}
+
+func main() {
+	two := student{
+		id:    111,
+		name:  "mir",
+		marks: 233.44,
+	}
+
 	fmt.Println(two)
-	fmt.Println(one.name, two.name)
+
+	newtwo := 276.55
+	two.changeMarks(newtwo)
+
+	fmt.Println(two.marks)
+
+	newone := newStudent(2, "shaukat", 322)
+
+	fmt.Println(newone)
+
+	department := struct {
+		name      string
+		employees int
+	}{"devops", 43}
+
+	fmt.Println(department)
 }
